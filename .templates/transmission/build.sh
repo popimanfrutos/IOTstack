06f2156c1abe146e6b35a3013dcdece913752de5
cayetano <cayetano@github.com>
2021-05-24 12:15:09 +0100

    MeschCetral Build

--- .templates/transmission/build.sh
+++ .templates/transmission/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+base_dir=$1
 # create directories for named volumes
-TRANSMISSION_BASEDIR=./volumes/transmission
+TRANSMISSION_BASEDIR=$base_dir/transmission
 mkdir -p $TRANSMISSION_BASEDIR/downloads
 mkdir -p $TRANSMISSION_BASEDIR/watch
 mkdir -p $TRANSMISSION_BASEDIR/config
